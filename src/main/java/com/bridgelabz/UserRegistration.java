963779478609822405519670ab5fa9a2b9fbb06f
dada4747 <adsurerahul96@gmail.com>
2021-05-28 21:56:53 +0530

    ADDED:Test for password has min 8 char

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
     private static final String MOBILE_PATTERN = "^[0-9]{0,2}[ ][0-9]{10}";
+    private static final String PASSWORD_PATTERN = "(?=.*[A-Z]+)(?=.*[0-9]+).{8,}";
 
     public boolean validateFirstName(String fName) {
         return patternChecker(fName, NAME_PATTERN);
@@ -18,6 +19,9 @@ public class UserRegistration {
     }
     public boolean validateMobileNum(String mobileNum){
         return patternChecker(mobileNum, MOBILE_PATTERN);
+    }
+    public boolean validatePassword(String password){
+        return patternChecker(password, PASSWORD_PATTERN);
     }
     private boolean patternChecker(String input,String fieldPattern) {
         Pattern pattern = Pattern.compile(fieldPattern);
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -76,6 +76,11 @@ public class UserRegistrationTest {
         boolean result = userRegistration.validateMobileNum("1234507748");
         Assertions.assertFalse(result);
     }
+    @Test
+    public void givenPassword_WhenMin8Chars_ShouldReturnTrue() {
+        boolean result = userRegistration.validatePassword("nDj1k2*jf");
+        Assertions.assertTrue(result);
+    }
 
 
 }
